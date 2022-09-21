4e761829ce9e8046b78777ae36ac1d0ef2f30759
Alejandro Henao <alejandrohumanld.io>
2022-09-20 21:07:15 -0500

    First commit

---
+++ ETL.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+
+from pathlib import Path
+
+p = Path("/home/ahenao/Documents/twitter_selenium/TweetScraper/Output/")
+
+tweets = (p / "MDGBI_tweets.json")
+# users (p.raw / "Scribe_users_merged.json")
+
+with open(tweets, 'r') as f:
+    data = f.read()
+    data_json = json.loads(data)
+te = [t['raw_data'] for t in data_json]
+df = pd.DataFrame(te)
+
+cols = ['created_at', 'id', 'full_text', 'geo', 'coordinates',
+        'retweet_count', 'favorite_count', 'reply_count',
+       'quote_count', 'favorited', 'retweeted', 'possibly_sensitive',
+       'lang']
+
+df[cols].to_parquet(Path("./data/") / "MDGBI_tweets.parquet")
